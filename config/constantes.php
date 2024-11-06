6e15c9b0484cf53ece69d2bde95ab01eb02fad06
Gorka_DALMAYRAC-BELASCAIN <gdbelascain@iutbayonne.univ-pau.fr>
2024-11-06 15:06:39 +0100

    ajout d'un template de base et de la config de twig

---
+++ config/constantes.php
@@ -0,0 +1,14 @@
+<?php
+//Connexion à la base de données
+define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
+define('DB_NAME', 'gdbelascain_bd');
+define('DB_USER', 'gdbelascain_bd');
+define('DB_PASS', 'gdbelascain_bd');
+
+define('PREFIXE_TABLE', 'bookngo_');
+
+// Constante de la partie vue
+define('WEBSITE_TITLE', 'Book\'n\'Go');
+define('WEBSITE_VERSION', '2.5');
+define('WEBSITE_DESCRIPTION', 'Book\'n\'Go : application de carnets de voyage');
+define('WEBSITE_LANGUAGE', 'fr');
