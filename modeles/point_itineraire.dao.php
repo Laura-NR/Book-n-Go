5f9dcfea8009e3d8b5f4a7b02bba22252e94c2e3
Yoann Gomes <ygomes@iutbayonne.univ-pau.fr>
2024-11-08 10:08:00 +0100

    Ajout de la classe Point Itineraire

---
+++ modeles/point_itineraire.dao.php
@@ -0,0 +1,74 @@
+<?php
+    class PointItineraireDao{
+        private ?PDO $pdo;
+
+        //Constructeur
+        public function __construct(?PDO $pdo = null){
+            $this->pdo = $pdo;
+        }
+
+        //Getteur
+        public function getPdo(){
+            return $this->pdo;
+        }
+
+        //Setteur
+        public function setPdo(?PDO $pdo = null){
+            $this->pdo = $pdo;
+        }
+
+        public function find(?int $id): ?PointItineraire{
+            $sql ="SELECT * FROM point_itineraire WHERE id= :id";
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"point_itineraire");
+            $pointItineraire = $pdoStatement->fetch();
+            return $pointItineraire;
+        }
+
+        public function findAll(){
+            $sql ="SELECT * FROM point_itineraire";
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"point_itineraire");
+            $pointItineraire = $pdoStatement->fetchAll();
+            return $pointItineraire;
+        }
+
+        public function findAssoc(?int $id): ?array{
+            $sql ="SELECT * FROM point_itineraire WHERE id= :id";
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            $pointItineraire = $pdoStatement->fetch();
+            return $pointItineraire;
+        }
+
+        public function findAllAssoc(){
+            $sql ="SELECT * FROM point_itineraire";
+            $pdoStatement = $this->pdo->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            $pointItineraire = $pdoStatement->fetchAll();
+            return $pointItineraire;
+        }
+
+        public function hydrate($tableauAssoc): ?PointItineraire{
+            $pointItineraire = new PointItineraire();
+            $pointItineraire->setId($tableauAssoc["id"]);
+            $pointItineraire->setAddress($tableauAssoc["id"]);
+            $pointItineraire->setDescription($tableauAssoc["id"]);
+            $pointItineraire->setTitre($tableauAssoc["id"]);
+            return $pointItineraire;
+        }
+
+        public function hydrateAll($tab): ?array{
+            $pointItineraireTab = [];
+            foreach ($tab as $tableauAssoc) {
+                $pointItineraire = $this->hydrate($tableauAssoc);
+                $pointItineraireTab[] = $pointItineraire;
+            }
+            return $pointItineraireTab;
+        }
+    }
+?>
