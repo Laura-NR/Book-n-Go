f436f1e5fba3e58177438f0ab90f4ec2596dfa5a
Kamelia Hamchouche <hamchouchek65@gmail.com>
2024-11-20 12:35:42 +0100

    petite modif de pageInformations aux niveau des commentaire

--- pageInformation.php
+++ pageInformation.php
@@ -1,7 +1,7 @@
 <?
 // inclure les fichier dont nous avons besoins
 require_once 'include.php';
-echo"<h1> test de la page d'informations</h1>";
+echo"<h1> test de la page d'informations des guides</h1>";
 
 // Charger et afficher le template
 // je veux charger le template de la page d'accueil 
