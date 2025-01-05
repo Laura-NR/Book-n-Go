255e4d44a834ef77bd4704676365c19fa0d91dab
imane <imanemahssini2@gmail.com>
2025-01-05 19:47:37 +0100

    ajout de la fonctionnalité de suppresion pour les reservations

--- controllers/controller.reservation.php
+++ controllers/controller.reservation.php
@@ -22,8 +22,6 @@ class ControllerReservation extends BaseController {
         ]);
     }
 
-
-
     public function creer(): void
     {
         $idExcursion = $_POST['id_excursion'];
@@ -41,6 +39,25 @@ class ControllerReservation extends BaseController {
         } else {
             echo "Erreur lors de la création de la réservation.";
         }
+    }
+    public function supprimerReservation(): void
+    {
+        // Récupérer l'ID de la réservation depuis l'URL ou le formulaire
+        $id_reservation = $_GET['id'] ?? null;
+
+        if (!$id_reservation) {
+            throw new Exception("Paramètre manquant : id_reservation");
+        }
+
+        // Instanciation du DAO
+        $reservationDAO = new ReservationDAO($this->getPdo());
+
+        // Appel de la méthode de suppression
+        $reservationDAO->supprimerReservation((int)$id_reservation);
+
+        // Redirection après la suppression (vers la liste ou autre page)
+        header("Location: index.php?controleur=reservation&methode=lister");
+        exit;
     }
 
 }
