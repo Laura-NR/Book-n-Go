e3f201dbc617085d63fa2a3c9bedf3987ac5ab17
GENYST\laura <nunez.laura.e@protonmail.com>
2025-02-25 15:57:34 +0100

    Visibilité du titre "Visites Selectionnées" corrigé. Ajout d'un modal de confirmation pour la suppresion des excursions

---
+++ js/guide_excursions.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Hello Everyone');
+    // Les notifications vont disparaitre après 5 secondes si l'utilisateur ne les ferme pas
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(alert => {
+        const bsAlert = new bootstrap.Alert(alert);
+        setTimeout(() => {
+            bsAlert.close();
+        }, 5000);
+    });
+
+    // Code pour le modal de suppression
+    const deleteModal = document.getElementById('deleteModal');
+    const deleteLinks = document.querySelectorAll('.delete-link');
+    console.log('Hello_1');
+    const confirmDeleteButton = document.getElementById('confirmDeleteButton');
+
+    
+    deleteLinks.forEach(deleteLink => {
+        deleteLink.addEventListener('click', function(event) {
+            console.log('click');
+            event.preventDefault();
+            const excursionId = this.getAttribute('data-id');
+            console.log(excursionId);
+            confirmDeleteButton.href = `index.php?controleur=excursion&methode=supprimer&id=${excursionId}`;
+        });
+    });
+});
